da38d8c50d044b538f23c17d0608e7a08730b209
seanhuxy <huxueyangs@gmail.com>
2015-12-13 16:19:59 -0500

    add common.go and drivers for client and server

---
+++ src/common.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "log"
+)
+
+var (
+    Debug *log.Logger = log.New(os.Stderr, "Debug: ", log.Lshortfile)
+    Error *log.Logger = log.New(os.Stderr, "Error: ", log.Lshortfile)
+)
+
+const (
+    DEF_BUF_SIZE int = 1500
+    DEF_TOP_DOMAIN string = "b.jannotti.com"
+    DEF_DOMAIN_PORT string = ":53"
+)
