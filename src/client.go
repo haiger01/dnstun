e19c1b4e62f3df0bb05c6912db78c312a031495a
seanhuxy <huxueyangs@gmail.com>
2015-12-12 23:51:34 -0500

    client skeleton code

--- src/client.go
+++ src/client.go
@@ -14,80 +14,159 @@ var (
     Error *log.Logger = log.New(os.Stderr, "Error: ", log.Lshortfile)
 )
 
-func OpenTUN() (*water.Interface){
-    ifce, err := water.NewTUN("tun66")
+const (
+    DEF_BUF_SIZE int = 1500
+)
+
+type Client struct {
+    ClientVAddr     *IPAddr
+    ServerVAddr     *IPAddr
+
+    Laddr           string  // local address
+    LocalServer     string  // local dns server
+    TopDomain       string
+
+    DNSConn         *net.UDPConn
+    TUNConn         *water.Interface
+
+    Buffer          map[int][]byte
+
+    Running         bool
+}
+
+func NewClient(topdomain string, ldns string) error {
+
+    c := new(Client)
+    c.TopDomain = topdomain
+    c.Laddr = ...   // TODO
+    c.LocalServer = ldns
+
+    c.Running = false
+
+    c.ClientVAddr = nil
+    c.ServerVAddr = nil
+    c.DNSConn = nil
+    c.TUNConn = nil
+
+    c.Buffer = make(map[int][]byte)
+}
+
+func (c *Client) Connect() error {
+
+    // connect with UDP
+    DNSConn, err = net.ListenUDP("udp4", c.Laddr) // TODO
     if err != nil {
-        panic(err)
+        Error.Printl
     }
-    return ifce
+
+    // send connect request
+    tunCmd = new(TUNPacket)
+    tunCmd.Cmd = TUN_CMD_CREATE
+    dnsPacket, err := c.dns.InjectTunCmd(tunCmd)
+    c.DNSSend(dnsPacket)
+
+    go c.DNSRecv()
 }
 
-func OpenDNSClient() (*dns.Client){
+func (c *Client) DNSSend(pkt []byte) error{
 
-    c := new(dns.Client)
-    c.Net = "udp"
-
-    return c
+    _, err :=  DNSConn.Write(pkt)
+    return err
 }
 
-func Recv(){
+func (c *Client) TUNRecv(){
 
-    for{
-        // reading from DNS socket
+    b = make([]byte, DEF_BUF_SIZE )
+    for c.Running == true {
 
-        // decapsulate ip packet from dns
+        n, err := TUNConn.Read(b)
+        if err != nil {
+            Error.Println(err)
+            continue
+        }
 
-        // print
+        ipp, err := ip.Unmarshal(b[:n])
+        if err != nil {
+            Error.Println(err)
+            continue
+        }
+
+        written := 0
+        for written < n {
+            // encapsulate tunp to DNS Packet
+            dnsPacket, w, err := c.dns.Encapsulate(ipp, b, written)
+            c.DNSSend(dnsPacket)
+            if err != nil {
+                Error.Println(err)
+                continue
+            }
+
+            written += w
+        }
     }
 }
 
-func Send(){
-    for{
-        // receive ip packet from TUN
+func (c *Client) Upload(pkt []byte) error{
+
+    n, err := c.TUNConn.Write(pkt)
+    if err != nil {
+        Error.Println(err)
+        return err
+    }
+    if n != len(pkt){
+        return fmt.Errorf("Short write %d, should be %d", n, len(pkt))
     }
 }
 
-func NewDNSPacket(raw []byte) *dns.Msg {
+func (c *Client) SaveTUNPacket(tun *TUNPacket){
 
-    msg := new(dns.Msg)
-    //msg.SetQuestion("www.google.com.", dns.TypeA)
-    msg.SetQuestion("b.jannotti.com.", dns.TypeA)
-    msg.RecursionDesired =true
-    txt := new(dns.TXT)
-    txt.Txt = []string{string(raw)}
-
-    //msg.Insert([]dns.RR{txt})
-
-    Debug.Println(msg.String())
-    return msg
+    if tun.Offset == 0 && tun.More == false {
+        pkt := tun.Payload
+        c.Upload(pkt)   // send to upper layer
+        return
+    }
+    pkt, ok := c.Buffer[tun.Id]
+    if ok {
+        if tun.Offset == len(pkt) {
+            pkt := append(pkt, tun.Payload)
+            if tun.More == false{
+                c.Upload(pkt)
+                delete(c.Buffer, tun.Id)
+            }else{
+                c.Buffer[tun.Id] = pkt
+            }
+        }
+    }else{
+        c.Buffer[tun.Id] = tun.Payload
+    }
 }
 
-func main(){
+func (c *Client) DNSRecv(){
 
-    // open TUN connection
-    ifce := OpenTUN();
-    client := OpenDNSClient();
-
-    buffer := make([]byte, 1600)
+    b := make([]byte, DEF_BUF_SIZE)
     for {
-        n, err := ifce.Read(buffer)
-        if err != nil {
-            Error.Println(err)
-        }
-        fmt.Printf("Read %d bytes\n", n)
-
-        // encapsulate into dns packet
-        msg := NewDNSPacket(buffer[:n])
-
-        // send out dns TODO: local dns server
-        res, rtt, err := client.Exchange(msg, "172.31.0.2:53")
-        if err != nil {
+        n, addr, err := c.DNSConn.ReadFrom(b)
+        if err != nil{
             Error.Println(err)
         }
 
-        Debug.Printf("DNS response, rtt: %s : %s\n", rtt.String(), res.String())
+        tun := tunnel.Unmarshal(b)
+        switch tun.Cmd {
+        case TUN_CMD_RESPONSE:
+            cmd := tun.ToCmdPacket()
+            c.ServerVAddr = cmd.Server
+            c.ClientVAddr = cmd.Client
+            c.Running = true
+            go c.TUNRecv()
+
+        case TUN_CMD_DATA:
+            if c.Running == true{
+                c.SaveTUNPacket(tun)
+            }
+        default:
+            Error.Println("Invalid TUN Cmd")
+        }
     }
 }
-
 
 
